53258f60f40147b7691c01841bc75ac12c9c6af4
Pedif <foroughipedram@gmail.com>
2025-12-31 12:49:10 +0330

    Add Dimens to design system

---
+++ client/android/imageProcessor/designSystem/src/main/java/com/techys/ip/designsystem/theme/Dimen.kt
@@ -0,0 +1,15 @@
+package com.techys.ip.designsystem.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimen {
+
+    val paddingSmall = 4.dp
+    val paddingMedium = 8.dp
+    val paddingLarge = 12.dp
+    val paddingXLarge = 16.dp
+    val paddingXXLarge = 20.dp
+
+    val paddingScreenVertical = 16.dp
+    val paddingScreenHorizontal = 16.dp
+}
