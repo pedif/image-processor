6bf9adc79e97e8ef3f193af808d33a365baeea2c
Pedif <foroughipedram@gmail.com>
2026-02-07 09:10:52 +0330

    Add server upload test
    Added an automated test to check the upload function of the backend

--- server/tests/test_upload.py
+++ server/tests/test_upload.py
@@ -1,16 +1,26 @@
-import requests
+from fastapi.testclient import TestClient
+from api.main import app
+
+client = TestClient(app)
+
 
 # URL of local FastAPI endpoint
 url = "http://127.0.0.1:8000/upload"
 
 # Path to test image
 image_path = "uploads/test.png"
+image_name = "test.png"
 
-# Open the file in binary mode and send POST request
-with open(image_path, "rb") as f:
-    files = {"file": f}
-    response = requests.post(url, files=files)
+def test_image_upload():
+    with open(image_path, "rb") as f:
+        response = client.post(
+            "upload/",
+            files = {"file": f}
+        )
+        assert response.status_code == 200
 
-# Print the JSON response
-print(response.status_code)
-print(response.json())
+        data = response.json()
+        assert data["filename"] == "test.png"
+        assert "result" in data
+        assert "width" in data["result"]
+        assert "height" in data["result"]
