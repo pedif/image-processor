781cdab74a0cf9752a04a3251b9a70f8d7843cd9
Pedif <foroughipedram@gmail.com>
2026-02-08 14:25:39 +0330

    server: add file validators
    we now validate the uploaded file so it does not crash our server

--- server/api/main.py
+++ server/api/main.py
@@ -1,23 +1,35 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
 import inference.processing
 import logging
 import asyncio
+from api.validators import check_file_size, check_image_validity
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI(title="ImageProcessor API")
 
-
 @app.get("/")
-def home():
+async def home():
     return {"message": "Hello, ImageProcessor!"}
+
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
 
 
 @app.post("/upload")
 async def upload_image(file: UploadFile = File(...)):
     contents = await file.read()
     logger.info(f"Received file: {file.filename}")
+
+    # Checking the validity of the uploaded file before starting the processing
+    check_file_size(contents)
+    check_image_validity(contents)
+
     loop = asyncio.get_running_loop()
-    result = await loop.run_in_executor(None,inference.processing.process_image, contents )
+    result = await loop.run_in_executor(
+        None, inference.processing.process_image, contents
+    )
     return result
---
+++ server/api/validators.py
@@ -0,0 +1,24 @@
+from fastapi import HTTPException
+from io import BytesIO
+from PIL import Image
+
+
+# We don't want to accept big files in case they crash our server
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+
+
+# File Size Check
+def check_file_size(contents: bytes):
+    if len(contents) > MAX_FILE_SIZE:
+        raise HTTPException(status_code=400, detail="File too large")
+
+
+# Image Validation
+def check_image_validity(contents: bytes):
+    try:
+        image = Image.open(BytesIO(contents))
+        # Validate image integrity
+        image.verify()
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid image file")
+
