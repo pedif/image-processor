1a8a132cd55a42d49e9f892bbe0c9c333b95112f
Pedif <foroughipedram@gmail.com>
2026-02-15 09:09:24 +0330

    client: add proxy setup for gradle
    The gradle now uses a setup socks proxy if available

--- client/android/imageProcessor/build.gradle.kts
+++ client/android/imageProcessor/build.gradle.kts
@@ -1,4 +1,21 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+import java.util.Properties
+
+// In case your system is using a proxy setup
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localPropertiesFile.inputStream().use { localProperties.load(it) }
+}
+localProperties.getProperty("socksProxyHost")?.let {
+    System.setProperty("socksProxyHost", it)
+}
+
+localProperties.getProperty("socksProxyPort")?.let {
+    System.setProperty("socksProxyPort", it)
+}
+//
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
