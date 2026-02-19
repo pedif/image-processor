4564a37a102f477b1534af5d4a6255642dd4c1df
Pedif <foroughipedram@gmail.com>
2026-02-19 17:37:20 +0330

    client: added more error messages

--- client/android/imageProcessor/classification/src/main/java/com/techys/classification/mapper/ErrorMapper.kt
+++ client/android/imageProcessor/classification/src/main/java/com/techys/classification/mapper/ErrorMapper.kt
@@ -9,11 +9,21 @@ import com.techys.ip.domain.model.ClassificationError
  * @return @StringRes
  */
 fun ClassificationError.getMessage(): Int {
-   return when (this) {
+    return when (this) {
         ClassificationError.FileNotFound -> R.string.error_classification_file_not_found
         ClassificationError.Timeout -> R.string.error_classification_timeout
-        ClassificationError.ModelCrashed -> R.string.error_classification_crash
-        else ->
+        ClassificationError.ProcessingCrash -> R.string.error_classification_crash
+        ClassificationError.Network -> R.string.error_classification_network
+        is ClassificationError.UnknownServerError -> {
+            R.string.error_classification_server_crash
+        }
+
+        ClassificationError.InvalidImageFormat, ClassificationError.FileTooLarge -> {
+            R.string.error_classification_image_invalid
+        }
+
+        is ClassificationError.Unknown -> {
             R.string.error_classification_unknown
+        }
     }
 }
